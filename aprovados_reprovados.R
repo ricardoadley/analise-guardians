58194b6f32298c8460244bc33b4e3a60f8875ca4
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2021-09-20 20:08:56 -0300

    nova tabela para aprovados e reprovados

--- aprovados_reprovados.R
+++ aprovados_reprovados.R
@@ -14,14 +14,16 @@ rep_e_nao_graduou <- loac %>%
   filter((situacao == "Reprovado por Falta" | situacao == "Trancado" | situacao == "Reprovado") & forma_evasao != "GRADUADO")%>%
   summarize(reprovados = n_distinct(id))
 
-#construcao do data frame
+#construcao do data frame para aprovacoes e reprovacoes na disciplina
 rep_apv <- data.frame (
   aprovacoes,
   reprovacoes
 )
-rep_apv$rep_sem_gradu <- rep_e_nao_graduou$reprovados
-rep_apv$total <- c(rep_apv$aprovacoes + rep_apv$rep_sem_gradu)
-rep_apv$pct_reprovados <- c(((100*rep_apv$rep_sem_gradu)/rep_apv$total))
-rep_apv$pct_aprovados <- c(((100*rep_apv$aprovacoes)/rep_apv$total))
-rep_apv$pct_reprovacao <- c(((100*rep_apv$reprovacoes)/rep_apv$total))
+#rep_apv$rep_sem_gradu <- rep_e_nao_graduou$reprovados
+#rep_apv$total <- c(rep_apv$aprovacoes + rep_e_nao_graduou$reprovados)
+rep_apv$pct_reprovacoes <- c(((100*rep_apv$reprovacoes)/(rep_apv$aprovacoes+rep_apv$reprovacoes)))
+rep_apv$pct_aprovacoes <- c(((100*rep_apv$aprovacoes)/(rep_apv$aprovacoes+rep_apv$reprovacoes)))
+#View(rep_apv)
+
+#rep_apv$pct_reprovacao <- c(((100*rep_apv$reprovacoes)/rep_apv$total))
 
